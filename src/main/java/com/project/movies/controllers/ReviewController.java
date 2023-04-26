f9a2dcd73204b3bb48709659fd05f160e3ed80ac
souvikbasak97 <souvikbasak97@gmail.com>
2023-04-26 23:30:29 +0530

    Initial commit

--- src/main/java/com/project/movies/controllers/ReviewController.java
+++ src/main/java/com/project/movies/controllers/ReviewController.java
@@ -8,13 +8,10 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Repository;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
-
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/v1/reviews")
 public class ReviewController  {
